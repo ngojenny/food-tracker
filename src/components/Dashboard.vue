3601db7304310cda0639a40b17f79a98c491eb06
Jenny Ngo <jennyngo.dev@gmail.com>
2019-11-18 13:51:24 -0500

    initial commit, create DayEntry component and Dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="wrapper">
+    <h2>Dashboard {{text}}</h2>
+    <div v-for="seed in seeds" v-bind:key="seed.date">
+      <DayEntry
+        :date="seed.date"
+        :foods="seed.foods"
+        :gut="seed.gut"
+        :skin="seed.skin"
+        :tags="seed.tags"
+        :notes="seed.notes"
+      />
+    </div>
+    <button v-on:click="toggleEntryForm">New Entry</button>
+  </div>
+</template>
+
+<script>
+import DayEntry from "./DayEntry.vue";
+export default {
+  name: "Dashboard",
+  components: {
+    DayEntry
+  },
+  data() {
+    return {
+      text: "Component",
+      seeds: [
+        {
+          date: "Jan 1, 2019",
+          foods: [
+            "oatmeal with milk",
+            "apple",
+            "cheeseburger",
+            "chocolate bar",
+            "spaghetti"
+          ],
+          gut: "bloated by EOD",
+          skin: "clear",
+          tags: ["bloated", "clear-skin"],
+          notes: "started using new moisturizer"
+        },
+        {
+          date: "Jan 2, 2019",
+          foods: [
+            "eggs and bacon",
+            "apple",
+            "kale salad",
+            "chocolate bar",
+            "spaghetti"
+          ],
+          gut: "bloated by EOD",
+          skin: "mild acne",
+          tags: ["bloated", "clear-skin"],
+          notes: ""
+        },
+        {
+          date: "Jan 3, 2019",
+          foods: [
+            "eggs and bacon",
+            "coffee",
+            "banana",
+            "chicken soup",
+            "chocolate bar",
+            "salmon and asparagus"
+          ],
+          gut: "bloated by EOD",
+          skin: "mild acne",
+          tags: ["bloated", "clear-skin"],
+          notes: ""
+        }
+      ]
+    };
+  },
+  methods: {
+    toggleEntryForm() {
+      console.log("toggleEntryForm");
+    },
+    addEntry() {
+      console.log("adding entry!");
+    }
+  }
+};
+</script>
---
+++ src/components/DayEntry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <p>
+      <span class="entry-label">date:</span>
+      {{date}}
+    </p>
+    <p>
+      <span class="entry-label">food:</span>
+    </p>
+    <ul v-for="food in foods" v-bind:key="food">
+      <li>{{food}}</li>
+    </ul>
+    <p>
+      <span class="entry-label">gut:</span>
+      {{gut}}
+    </p>
+    <p>
+      <span class="entry-label">skin:</span>
+      {{skin}}
+    </p>
+    <p>
+      <span class="entry-label">tags:</span>
+    </p>
+    <ul v-for="tag in tags" v-bind:key="tag">
+      <li>{{tag}}</li>
+    </ul>
+    <p>
+      <span class="entry-label">notes:</span>
+      {{notes}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["date", "foods", "gut", "skin", "tags", "notes"]
+};
+</script>
