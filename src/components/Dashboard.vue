21d8cf21366e6d3000dc6f6065fd261e5a8071dc
Jenny Ngo <jennyngo.dev@gmail.com>
2019-11-19 16:55:23 -0500

    started filtering feature

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,16 +5,17 @@
       <button class="add-button" type="button" v-on:click="toggleEntryForm">New Entry ✎</button>
     </header>
     <main>
+      <FilterSortBar :entries="entries" />
       <div class="card-container">
         <DayEntry
-          v-for="seed in seeds"
-          v-bind:key="seed.date"
-          :date="seed.date"
-          :foods="seed.foods"
-          :gut="seed.gut"
-          :skin="seed.skin"
-          :tags="seed.tags"
-          :notes="seed.notes"
+          v-for="entry in entries"
+          v-bind:key="entry.date"
+          :date="entry.date"
+          :foods="entry.foods"
+          :gut="entry.gut"
+          :skin="entry.skin"
+          :tags="entry.tags"
+          :notes="entry.notes"
         />
       </div>
       <EntryForm v-if="formVisible" @send-entry="addEntry" @close-modal="toggleEntryForm" />
@@ -25,65 +26,67 @@
 <script>
 import DayEntry from "./DayEntry.vue";
 import EntryForm from "./EntryForm.vue";
+import FilterSortBar from "./FilterSortBar.vue";
 export default {
   name: "Dashboard",
   components: {
     DayEntry,
-    EntryForm
+    EntryForm,
+    FilterSortBar
   },
   data() {
     return {
       formVisible: false,
-      seeds: [
+      entries: [
         {
           date: "Jan 1, 2019",
           foods: [
-            { description: "oatmeal with milk", id: "seed1fooditem1" },
-            { description: "apple", id: "seed1fooditem2" },
-            { description: "cheeseburger", id: "seed1fooditem3" },
-            { description: "chocolate bar", id: "seed1fooditem4" },
-            { description: "spaghetti", id: "seed1fooditem5" }
+            { description: "oatmeal with milk", id: "entry1fooditem1" },
+            { description: "apple", id: "entry1fooditem2" },
+            { description: "cheeseburger", id: "entry1fooditem3" },
+            { description: "chocolate bar", id: "entry1fooditem4" },
+            { description: "spaghetti", id: "entry1fooditem5" }
           ],
           gut: "bloated by EOD",
           skin: "clear",
           tags: [
-            { description: "bloated", id: "seed1tagitem1" },
-            { description: "clear-skin", id: "seed1tagitem2" }
+            { description: "bloated", id: "entry1tagitem1" },
+            { description: "clear-skin", id: "entry1tagitem2" }
           ],
           notes: "started using new moisturizer"
         },
         {
           date: "Jan 2, 2019",
           foods: [
-            { description: "eggs and bacon", id: "seed2fooditem1" },
-            { description: "apple", id: "seed2fooditem2" },
-            { description: "kale salad", id: "seed2fooditem3" },
-            { description: "chocolate bar", id: "seed2fooditem4" },
-            { description: "spaghetti", id: "seed2fooditem5" }
+            { description: "eggs and bacon", id: "entry2fooditem1" },
+            { description: "apple", id: "entry2fooditem2" },
+            { description: "kale salad", id: "entry2fooditem3" },
+            { description: "chocolate bar", id: "entry2fooditem4" },
+            { description: "spaghetti", id: "entry2fooditem5" }
           ],
           gut: "bloated by EOD",
           skin: "mild acne",
           tags: [
-            { description: "bloated", id: "seed2tagitem1" },
-            { description: "clear-skin", id: "seed2tagitem2" }
+            { description: "bloated", id: "entry2tagitem1" },
+            { description: "clear-skin", id: "entry2tagitem2" }
           ],
           notes: ""
         },
         {
           date: "Jan 3, 2019",
           foods: [
-            { description: "eggs and bacon", id: "seed3fooditem1" },
-            { description: "coffee", id: "seed3fooditem2" },
-            { description: "banana", id: "seed3fooditem3" },
-            { description: "chicken soup", id: "seed3fooditem4" },
-            { description: "chocolate bar", id: "seed3fooditem5" },
-            { description: "salmon and asparagus", id: "seed3fooditem6" }
+            { description: "eggs and bacon", id: "entry3fooditem1" },
+            { description: "coffee", id: "entry3fooditem2" },
+            { description: "banana", id: "entry3fooditem3" },
+            { description: "chicken soup", id: "entry3fooditem4" },
+            { description: "chocolate bar", id: "entry3fooditem5" },
+            { description: "salmon and asparagus", id: "entry3fooditem6" }
           ],
           gut: "bloated by EOD",
           skin: "mild acne",
           tags: [
-            { description: "bloated", id: "seed3tagitem1" },
-            { description: "clear-skin", id: "seed3tagitem2" }
+            { description: "bloated", id: "entry3tagitem1" },
+            { description: "clear-skin", id: "entry3tagitem2" }
           ],
           notes: ""
         }
@@ -96,7 +99,7 @@ export default {
     },
     addEntry(val) {
       console.log("adding entry!", val);
-      this.seeds.push(val);
+      this.entries.push(val);
     }
   }
 };
---
+++ src/components/FilterSortBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h2>Filter by:</h2>
+    <select name id>
+      <option v-for="food in allFoodItems" v-bind:key="food" value="food">{{food}}</option>
+    </select>
+    <!-- <pre>
+      {{allFoodItems}}
+    </pre>-->
+  </div>
+</template>
+<script>
+export default {
+  props: ["entries"],
+  computed: {
+    allFoodItems: function() {
+      const allFoodItems = this.entries.map(entry => {
+        return entry.foods.map(arr => arr.description);
+      });
+      const allFoodDesc = [].concat(...allFoodItems).filter((val, i, arr) => {
+        return arr.indexOf(val) === i;
+      });
+      return allFoodDesc;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+}
+</style>
